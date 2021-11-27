a535cfaf192954d65152724e51c1c1bf4d8bea27
inamanu <64574783+inamanu@users.noreply.github.com>
2021-11-27 11:24:25 +0530

    Add files via upload

---
+++ assign.sh
@@ -0,0 +1,21 @@
+#echo "THIS IS FOR HEAD COMMAND"
+#echo "==============================================="
+#head file1.txt
+echo "==============================================="
+echo "THIS IS FOR TAIL COMMAND"
+tail file1.txt
+#echo "==============================================="
+#echo "THIS IS FOR ZIP FILE"
+#tar -zcvf backup_1oct.tar.gz file1.txt file2.txt file3.txt file4.txt assign.sh
+
+#echo "This is for unzip file using tar command"
+#tar -zxvf backup_1oct.tar.gz
+
+#echo"this is for zip file using zip command"
+#zip -r hey.zip *
+
+#echo"this is for unzip file using zip command"
+#unzip hey.zip
+
+#echo"move zip file into another folder using mv command"
+#mv hey.zip backup
